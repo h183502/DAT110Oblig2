04172475f0950d0b5dbf038d41aac08eb7e3b19c
h183502 <heine.fjeld@gmail.com>
2019-02-26 12:30:48 +0100

    Updated SubscribeMsg

--- project2/src/no/hvl/dat110/messages/SubscribeMsg.java
+++ project2/src/no/hvl/dat110/messages/SubscribeMsg.java
@@ -5,4 +5,23 @@ public class SubscribeMsg extends Message {
 	// TODO: 
 	// Implement objectvariables, constructor, get/set-methods, and toString method
 
+    private String subscribe;
+
+    public SubscribeMsg(String subscribe, String user){
+        super(MessageType.SUBSCRIBE, user);
+        this.subscribe = subscribe;
+    }
+
+    public String getSubscribe() {
+        return subscribe;
+    }
+
+    public void setSubscribe(String subscribe) {
+        this.subscribe = subscribe;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Subscription = " + this.subscribe;
+    }
 }
