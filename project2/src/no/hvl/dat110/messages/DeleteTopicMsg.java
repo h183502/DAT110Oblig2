75f279232bacc77f2c5c5e258a8519c8e8ae4986
h183502 <heine.fjeld@gmail.com>
2019-02-26 12:25:05 +0100

    Updated DeleteTopicMsg

--- project2/src/no/hvl/dat110/messages/DeleteTopicMsg.java
+++ project2/src/no/hvl/dat110/messages/DeleteTopicMsg.java
@@ -1,8 +1,27 @@
 package no.hvl.dat110.messages;
 
+
 public class DeleteTopicMsg extends Message {
 	
 	// TODO: 
 	// Implement objectvariables, constructor, get/set-methods, and toString method
-	
+
+    private String topic;
+
+    public DeleteTopicMsg(String user, String topic){
+        super(MessageType.DELETETOPIC, user);
+        this.topic = topic;
+    }
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " deleted topic = " + this.topic;
+    }
 }
