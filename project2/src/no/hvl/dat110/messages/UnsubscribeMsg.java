6b0adf4ba3a25d98f437cae618e7dead394dfa39
h183502 <heine.fjeld@gmail.com>
2019-02-26 12:33:10 +0100

    Updated UnSubscribeMsg

--- project2/src/no/hvl/dat110/messages/UnsubscribeMsg.java
+++ project2/src/no/hvl/dat110/messages/UnsubscribeMsg.java
@@ -4,5 +4,24 @@ public class UnsubscribeMsg extends Message {
 
 	// TODO: 
 	// Implement objectvariables, constructor, get/set-methods, and toString method
-	
+
+    private String subscrive;
+
+    public UnsubscribeMsg(String user, String subscrive){
+        super(MessageType.UNSUBSCRIBE, user);
+        this.subscrive = subscrive;
+    }
+
+    public String getSubscrive() {
+        return subscrive;
+    }
+
+    public void setSubscrive(String subscrive) {
+        this.subscrive = subscrive;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Unsubscrive = " + this.subscrive;
+    }
 }
